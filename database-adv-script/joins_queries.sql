d320a3f0d76b033f55277abd5d7a36cb38184884
LAPTOP-LDL7JQH2\Legion <yahianagib112@gmail.com>
2025-11-03 22:02:07 +0200

    modify

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,13 +1,14 @@
 SELECT * FROM Booking b
 INNER JOIN User u
-ON b.user_id = u.user_id
+ON b.user_id = u.user_id;
 
 
 SELECT * FROM Property p
 LEFT JOIN Review r
 ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 
 SELECT * FROM User u
 FULL OUTER JOIN Booking b
-ON b.user_id = u.user_id
+ON b.user_id = u.user_id;
