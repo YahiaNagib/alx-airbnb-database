358fd00333c9743952aab3286a0ee70985435b2f
LAPTOP-LDL7JQH2\Legion <yahianagib112@gmail.com>
2025-11-09 09:44:02 +0200

    m

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,7 +1,7 @@
 SELECT COUNT(*) FROM Booking
 GROUP BY user_id
 
-SELECT RANK() OVER (ORDER BY 
-(SELECT COUNT(*) FROM Booking b WHERE p.property_id = b.property_id  ) ) DESC AS property_rank
+SELECT RANK() ROW_NUMBER() OVER (ORDER BY 
+(SELECT COUNT(*) FROM Booking b WHERE p.property_id = b.property_id  )) DESC AS property_rank
 
 FROM Property p;
